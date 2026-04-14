1ee64a09921f79824e4f014b45e204d29592b8a6
ponzudomo <eh240393@meiji.ac.jp>
2026-04-14 12:47:19 +0900

    solved abc453_a and abc453_b

---
+++ A/abc453_a.cpp
@@ -0,0 +1,68 @@
+/**
+ * @file
+ * ABC453_A Trimo
+ * https://atcoder.jp/contests/abc453/tasks/abc453_a
+ * 
+ * (状況)
+ * 
+ * @brief
+ * (主に使ったアルゴリズム)
+ *
+ * @note
+ * Problem Statement:
+ * 
+ * Constraints:
+ * 
+ * Input:
+ * 
+ * Output:
+ * 
+ * @todo
+ *
+ */
+
+// include
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#include <boost/dynamic_bitset.hpp>
+using namespace boost;
+
+// define
+#define fore(x, a) for (auto &x : a)
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, m, n) for (int i = (int)(m); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+// typedef
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<string> vs;
+
+const double pi = 3.141592653589793238;
+const int inf = 1073741823;
+const ll infl = 1LL << 60;
+const int mod = 998244353;
+struct Init { Init() { ios::sync_with_stdio(0); cin.tie(0); } }init;
+
+
+int main() {
+  int n;
+  string s;
+  cin >> n >> s;
+  string ans = "";
+  int i = 0;
+  while(s[i] == 'o') i++;
+  while(i < n) {
+    ans += s[i];
+    i++;
+  }
+  cout << ans << endl;
+  return 0;
+}
